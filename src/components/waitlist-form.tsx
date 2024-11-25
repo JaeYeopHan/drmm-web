bd67e599985138cfc78e9c5f7688e30ab3017b4f
JaeYeopHan <ljyhanll@gmail.com>
2024-11-25 18:27:13 +0900

    fix: form

--- src/components/waitlist-form.tsx
+++ src/components/waitlist-form.tsx
@@ -11,14 +11,16 @@ export function WaitlistForm({ className }: { className?: string }) {
     <form className={cn("mx-auto mt-6 flex max-w-md gap-x-4", className)} action="https://formspree.io/f/xovqnygn" method="POST" onSubmit={() => {
       toast("감사합니다. 베타 준비되는대로 메일로 안내드릴게요.")
     }}>
-      <Input
-        type="email"
-        placeholder="이메일을 알려주세요"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        className="min-w-0 flex-auto"
-      />
+      <label className="min-w-0 flex-auto">
+        <Input
+          type="email"
+          name="email"
+          placeholder="이메일을 알려주세요"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
       <Button type="submit">
         베타 신청
       </Button>
